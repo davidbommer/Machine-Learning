9d03624a939b2aa2008050402f12dc65ae846c63
davidbommer <davidbommersbach@gmail.com>
2020-01-13 16:48:33 -0600

    Update KernelRegression.py

--- KernelRegression.py
+++ KernelRegression.py
@@ -1,29 +1,20 @@
 # # Kernel Regression
 # 
-# Given a training dataset $\{x_i, y_i\}_{i=1}^n$, kernel regression approximates the unknown 
+# Given a training dataset {x_i, y_i\}, kernel regression approximates the unknown 
 # nolinear relation between $x$ and $y$ with a function of form
 # $$
-# y\approx f(x;w) = \sum_{i=1}^n w_i k(x, x_i),
+# y approx equals f(x, w) = sum from {i=1} to n {w_i k(x, x_i)}
 # $$
-# where $k(x, x')$ is a positive definite kernel specified by the users, and ${w_i}$ is a
+# where k(x, x') is a positive definite kernel specified by the users, and {w_i} is a
 # set of weights. 
 # We will use the simple Gaussian radius basis funciton (RBF) kernel,
-# $$
-# k(x, x') = exp(-\frac{||x-x'||^2}{2h^2}),
-# $$
-# where $h$ is a bandwith parameter. 
+# where h is a bandwith parameter. 
 # 
 # 
 # ### Step 1. Simulate a 1-dimensional dataset
-
-
-
-
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-#get_ipython().run_line_magic('matplotlib', 'inline')
-
 np.random.seed(100)
 
 ### Step 1: Simulate a simple 1D data ###
@@ -38,11 +29,6 @@ plt.show()
 # Now we have a dataset with 100 training data points. Let us calculate the kernel function. 
 # 
 # ### Step 2. Kernel function
-# 
-# Your task is to complete the following rbf_kernel function that takes two sets of points $X$ (of size $n$) and $X'$ (of size $m$) and the bandwidth $h$ and ouputs their pairwise  kernel matrix $K = [k(x_i, x_j)]_{ij}$, which is of size $n\times m$. We will represent input data as matrices, with $X=[x_i]_{i=1}^n \in R^{n\times 1}$ denoting the input features and $Y=[y_i]_{i=1}^n\in R^{n\times 1}$ the input labels.)
-
-# In[2]:
-
 
 import math 
 def rbf(xi, xj, h):
@@ -55,9 +41,6 @@ def rbf_kernel(X, Xp, h):
     # X: n*1 matrix
     # Xp: m*1 matrix
     # h: scalar value 
-                
-    ## TODO: please calculate the kernel matrix in the following:  
-    # (hint: you can write your own pairwise distance function, or cipy.spatial.distance.cdist)
     n = X.shape[0]
     m = Xp.shape[0]
     K = np.zeros((n, m))
@@ -67,40 +50,20 @@ def rbf_kernel(X, Xp, h):
     
     return K #n*m
 
-
-
-### evaluation: if your implementation is correct, you should expect the output is a 2X3 matrix
-# [[0.60653066 1.         0.60653066]
-# [0.13533528 0.60653066 1.        ]]
-k_test = rbf_kernel(np.array([[2],[1]]), np.array([[3],[2],[1]]), 1)
-print(k_test)
-    
-
-
+   
 # ### Step 3. The median trick for bandwith
-# The choice of the bandwidth $h$
-# A common way to set the bandwith $h$ in practice is the so called median trick,
-# which sets $h$ to be the median of the pairwise distance on the training data, that is
-# $$
-# h_{med} = median(\{||x_i - x_j||: i\neq j, ~~~~~~i,j =1, ..., n\}).
-# $$
+# The choice of the bandwidth h
+# A common way to set the bandwith h in practice is the so called median trick,
+# which sets h to be the median of the pairwise distance on the training data, that is
 # 
-# - Task: Compelete the median distance function. 
-# 
-
-# In[3]:
-
+# h_{med} = median(||x_i - x_j||: i!=j, i,j = 1,...,n).
 
 from scipy.spatial import distance
 
 def median_distance(X):
     # X: n*1 matrix
-     
-    #TODO: Calculate the median of the pairwise distance of $X$ below 
-    #(hint: use '[dist[i, j] for i in range(len(X)) for j in range(len(X)) if i != j]' to remove the diagonal terms; use np.median)
     dist = 0
     count = 0
-    
     for i in range(X.shape[0]):
         for j in range(i, X.shape[0]):
             if (i != j):
@@ -111,33 +74,17 @@ def median_distance(X):
     return h
 
 
-### Test your functions
-#evaluation: if your implementation is correct, your answer should be [2.0]
-h_test = median_distance(np.array([[1],[2],[4]]))
-print(h_test)
-
 
 # ### Step 4. Kernel regression
-# The weights ${w_i}$ are estimated by minimizing a regularized mean square error:
-# $$
-# \min_w {\left ( \sum_{i=1}^n (y_i - f(x_i; w))^2 \right) + \beta w^\top K w },     
-# $$
-# where $w$ is the column vector formed by $w=[w_i]_{i=1}^n$ and K is the kernel matrix.
+# The weights {w_i} are estimated by minimizing a regularized mean square error:
+# where w is the column vector formed by w=[w_i] from i=1 to n and K is the kernel matrix.
 # 
-# - Please derive the optimal solution of $w$ using matrix inverseion (no need to show the work) 
-# 
-# - Complete the following function to implement the calculation of $w$ 
-
-# In[4]:
-
 
 from numpy.linalg import inv
 def kernel_regression_fitting(xTrain, yTrain, h, beta=1):
     # X: input data, numpy array, n*1
     # Y: input labels, numpy array, n*1
-    
     #W=((K+BI)^-1)Y
-    # TODO: calculate W below (it is a n*1 matrix)
     K = rbf_kernel(xTrain, xTrain, h)
     n = xTrain.shape[0]
     I = np.identity(n)
@@ -145,23 +92,13 @@ def kernel_regression_fitting(xTrain, yTrain, h, beta=1):
     K = np.add(K, I*B)
     K = inv(K)
     W = np.matmul(K,yTrain)
-  
     return W
 
-### evaluating your code, the shape should be (100, 1) (check the values yourself)
-h = median_distance(xTrain)
-W_test = kernel_regression_fitting(xTrain, yTrain, h)
-print(W_test.shape)
 
 
 # ### Step 5. Evaluation and Cross Validation
 # 
 # We now need to evaluate the algorithm on the testing data and select the hyperparameters (bandwidth and regularization coefficient) using cross validation
-
-# In[5]:
-
-
-# Please run and read the following base code 
 
 def kernel_regression_fit_and_predict(xTrain, yTrain, xTest, h, beta):
     
@@ -199,14 +136,10 @@ plt.show()
 
 
 # ### Step 5.1. Impact of bandwith 
-# Run the kernel regression with regularization coefficient $\beta=1$ and 
-# bandwidth $h \in \{ 0.1 h_{med}, h_{med}, 10h_{med}\}$.
+# Run the kernel regression with regularization coefficient beta=1 and 
+# bandwidth h in {0.1*h_{med}, h_{med}, 10*h_{med}}.
 # 
-# - Task: Show the curve learned by different $h$. Comment on how $h$ influences the smoothness of $h$.
-# 
-
-# In[6]:
-
+# Show the curve learned by different h. 
 
 ### fitting on the training data ###
 beta = 1.
@@ -215,7 +148,7 @@ plt.figure(figsize=(12, 4))
 for i, coff in enumerate([0.1, 1., 10]):
     plt.subplot(1, 3, i+1)
     
-    ### TODO: run kernel regression with bandwith h = coff * h_med. 
+    #run kernel regression with bandwith h = coff * h_med. 
     yHatk_i =  kernel_regression_fit_and_predict(xTrain, yTrain, xTest, h_med*coff, beta)
     
     # visulization
@@ -231,11 +164,7 @@ plt.show()
 
 # ### Step 5.2. Cross Validation (CV)
 # Use 5-fold cross validation to find the optimal combination of 
-# $h$ and $\beta$ within $h\in \{0.1h_{med}, h_{med}, 10h_{med}\}$ and $\beta \in \{0.1, 1\}$.
-# - Task: complete the code of cross validation and find the best $h$ and $\beta$. Plot the curve fit with the optimal hyperparameters.
-
-# In[14]:
-
+# h and beta within h in {0.1*h_{med}, h_{med}, 10*h_{med}} and beta in {0.1, 1}.
 
 best_beta, best_coff = 1., 1.
 best_mse = 1e8
@@ -268,13 +197,7 @@ for beta in [0.1, 1]:
             h = median_distance(i_xTrain)
             i_yPred = kernel_regression_fit_and_predict(i_xTrain, i_yTrain, i_xValid, h*coff, beta)     
             mse.append((i_yValid - i_yPred)**2)
-            
-            
-        
         mse = np.mean(mse)
-        
-
-       
         # keep track of the combination with the best MSE
         if mse < best_mse:
             best_beta, best_coff = beta, coff
